7ca8bdf2779de148a3bcb3eecc3d326442bd43cf
ziara1 <143950916+ziara1@users.noreply.github.com>
2024-11-01 23:01:11 +0100

    Add files via upload

--- dom.cpp
+++ dom.cpp
@@ -2,60 +2,40 @@
 #include <vector>
 #include <algorithm>
 
+void bt(int i, int j, int nextj, int curr, std::vector<std::vector<int64_t>> &dp, int &k, std::vector<std::vector<int>> &matrix){
+    dp[i + 1][nextj] = std::max(dp[i + 1][nextj], dp[i][j]);
+    if (curr == k) return;
+    bt(i, j, nextj, curr + 1, dp, k, matrix);
+    if (!((j) & (1 << curr))){
+        if (curr < k - 1 && (!(j & (1 << (curr + 1))))){
+            dp[i][j] += matrix[curr][i] + matrix[curr + 1][i];
+            bt(i, j, nextj, curr + 2, dp, k, matrix);
+            dp[i][j] -= matrix[curr][i] + matrix[curr + 1][i];
+        }
+        dp[i][j] += matrix[curr][i] + matrix[curr][i + 1];
+        bt(i, j, nextj | (1 << curr), curr + 1, dp, k, matrix);
+        dp[i][j] -= matrix[curr][i] + matrix[curr][i + 1];
+    }
+}
+
 int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
     int n, k;
-    std::cin >> n;
-    std::cin >> k;
+    std::cin >> n >> k;
     std::vector<std::vector<int>> matrix(k, std::vector<int>(n));
-    for (int i = 0; i < k; i++) {
-        for (int j = 0; j < n; j++) {
+    int maxState = (1 << k);
+    std::vector<std::vector<int64_t>> dp (n + 1, std::vector<int64_t>(maxState, -1000000000000));
+    dp[0][0] = 0;
+    for (int i = 0; i < k; i++)
+        for (int j = 0; j < n; j++)
             std::cin >> matrix[i][j];
-        }
-    }
-
-    std::vector<std::vector<int>> dp(n, std::vector<int>(1 << k, 0));
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < (1 << k); j++) {
-            std::vector<std::vector<int>> currDp(k, std::vector<int>(2, 0)); // do obliczenia dp[i][j], pierwsza to maks wynik dotychczas a druga jego ciag
-            if (i > 0) {
-                currDp[0][0] = dp[i - 1][(1 << k) - 1];
-                currDp[0][1] = (1 << k) - 1;
-                if (j & (1 << 0) && currDp[0][0] < matrix[0][i] + matrix[0][i - 1] + dp[i - 1][((1 << k) - 1) & ~(1 << 0)]) { //
-                    currDp[0][0] = matrix[0][i] + matrix[0][i - 1] + dp[i - 1][((1 << k) - 1) & ~(1 << 0)];
-                    currDp[0][1] = ((1 << k) - 1) & ~(1 << 0);
-                }
-                currDp[1] = currDp[0];
-                if (j & (1 << 1) && j & (1 << 0) && currDp[1][0] < matrix[1][i] + matrix[0][i] + dp[i - 1][(1 << k) - 1]) {
-                    currDp[1][0] = matrix[1][i] + matrix[0][i] + dp[i - 1][(1 << k) - 1];
-                    currDp[1][1] = (1 << k) - 1;
-                }
-                if (j & (1 << 1) && currDp[1][0] < matrix[1][i] + matrix[1][i - 1] + currDp[0][0] - dp[i - 1][currDp[0][1]] + dp[i - 1][currDp[0][1] & ~(1 << 1)]) {
-                    currDp[1][0] = matrix[1][i] + matrix[1][i - 1] + currDp[0][0] - dp[i - 1][currDp[0][1]] + dp[i - 1][currDp[0][1] & ~(1 << 1)];
-                    currDp[1][1] = currDp[0][1] & ~(1 << 1);
-                }
-            }
-            else if (j & (1 << 1) && j & (1 << 0)) {
-                currDp[1][0] = std::max(0, matrix[0][0] + matrix[1][0]);
-            }
-
-
-            for (int l = 2; l < k; l++) {
-                currDp[l] = currDp[l - 1];
-                if (j & (1 << l) && j & (1 << (l - 1)) && currDp[l][0] < matrix[l][i] + matrix[l - 1][i] + currDp[l - 2][0]) {
-                    currDp[l][0] = matrix[l][i] + matrix[l - 1][i] + currDp[l - 2][0];
-                    currDp[l][1] = currDp[l - 2][1];
-                }
-                if (i > 0 && j & (1 << l) && currDp[l][0] < matrix[l][i] + matrix[l][i - 1] + currDp[l - 1][0] - dp[i - 1][currDp[l - 1][1]] + dp[i - 1][currDp[l - 1][1] & ~(1 << l)]) {
-                    currDp[l][0] = matrix[l][i] + matrix[l][i - 1] + currDp[l - 1][0] - dp[i - 1][currDp[l - 1][1]] + dp[i - 1][currDp[l - 1][1] & ~(1 << l)];
-                    currDp[l][1] = currDp[l - 1][1] & ~(1 << l);
-                }
-            }
-            dp[i][j] = currDp[k - 1][0];
-        }
-    }
-    std::cout << dp[n - 1][(1 << k) - 1] << "\n";
-
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < maxState; j++)
+            if (dp[i][j] > -1000000000000)
+                bt(i, j, 0, 0, dp, k, matrix);
+    std::cout << dp[n][0];
     return 0;
 }
+
+
